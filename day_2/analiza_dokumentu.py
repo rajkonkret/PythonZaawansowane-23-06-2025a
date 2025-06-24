895bc41da905c52f58d4fda492726e18109caa0f
rajkonkret <rajkonkret660@gmail.com>
2025-06-24 13:06:25 +0200

    [2 16]add analiza_dokumentu.py

---
+++ day_2/analiza_dokumentu.py
@@ -0,0 +1,43 @@
+from typing import List, Callable, Dict
+from collections import Counter
+from functools import partial
+import re  # wyrażenia regularne
+
+
+# tylko hinty, nie sprawdzają typów
+def process_documents(docs: List[str], analisys_fn: Callable[[str], Dict[str, int]]) -> Dict[str, int]:
+    """
+    Funkcja wyższego rzędu, analizuje listę dokumentów
+    :param docs:
+    :param analisys_fn:
+    :return:
+    """
+    aggregate_counter = Counter()
+    for doc in docs:
+        result = analisys_fn(doc)
+        aggregate_counter.update(result)
+    return dict(aggregate_counter)
+
+
+def word_frequency_analisys(text: str) -> Dict[str, int]:
+    """
+    Funkcja analizy tekstu  dokumentu
+    :param text:
+    :return: częstotliwość wystąpień słów
+    """
+    stopwords = {'the', 'and', 'this', 'that', 'with', 'from', 'which'}
+    words = re.findall(r"\b\w+\b", text.lower())
+    filtered_words = [w for w in words if len(w) > 3 and w not in stopwords]
+    return dict(Counter(filtered_words))
+
+
+def keyword_sentence_count(text: str, keyword: str = "text") -> Dict[str, int]:
+    """
+    funkcja liczy ile zdań w tekście zawiera wskazane słowa kluczowe
+    :param text:
+    :param keyword:
+    :return:
+    """
+    sentences = re.split(f'[.!?]', text)
+    count = sum(1 for sentence in sentences if keyword.lower() in sentence.lower())
+    return {keyword: count}
