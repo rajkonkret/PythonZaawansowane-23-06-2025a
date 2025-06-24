f18b6f60f795fe8811a4863aa1f044a9757c1c3d
rajkonkret <rajkonkret660@gmail.com>
2025-06-24 12:09:47 +0200

    [2 11] add klasa5_call.py

---
+++ day_2/klasa5_call.py
@@ -0,0 +1,25 @@
+import datetime
+# obiekt klasy stał się funkcją
+
+class Greeting:
+    def __call__(self, hour_=None):
+        if hour_ is None:
+            hour_ = datetime.datetime.now().hour
+
+        if 5 <= hour_ < 12:
+            return "Dzień dobry"
+        elif 12 <= hour_ < 18:
+            return "Miłego popołudnia"
+        elif 18 <= hour_ < 22:
+            return "Dobry wieczór"
+        else:
+            return "Dobranoc"
+
+
+greet = Greeting()
+print(greet)  # <__main__.Greeting object at 0x00000211B7C96E40>
+
+print(greet())  # Miłego popołudnia
+print(greet(10))  # Dzień dobry
+print(greet(17))  # Miłego popołudnia
+print(greet(23))  # Dobranoc
